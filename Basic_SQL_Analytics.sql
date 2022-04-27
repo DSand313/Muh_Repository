c5b89f57b9417747b5db47d56d2948ea7202ec76
James Sandusky <dsand313@gmail.com>
2022-04-27 18:53:31 -0500

    Added our first two SQL files

---
+++ Basic_SQL_Analytics.sql
@@ -0,0 +1,47 @@
+-- joins
+USE sql_store;
+SELECT *
+FROM orders o
+JOIN customers c
+	USING (customer_id)
+JOIN order_statuses os
+	ON o.status = os.order_status_id;
+
+-- subqueries
+USE sql_invoicing;
+SELECT * 
+FROM clients
+WHERE client_id IN (
+	SELECT client_id
+    FROM invoices
+    GROUP BY client_id
+    HAVING COUNT(*) > 2
+);
+
+-- correlated subqueries
+USE sql_hr;
+SELECT * 
+FROM employees e
+WHERE salary > (
+	SELECT AVG(salary)
+    FROM employees
+    WHERE office_id = e.office_id
+);
+
+-- unions
+USE sql_store;
+SELECT 
+	order_id,
+    order_date,
+    "Active" AS status
+FROM orders
+WHERE order_date >= "2019-01-01"
+UNION
+SELECT 
+	order_id,
+    order_date,
+    "Archived" AS status
+FROM orders
+WHERE order_date < "2019-01-01"
+
+
