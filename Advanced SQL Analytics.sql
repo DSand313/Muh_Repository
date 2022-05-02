9fdb7eab6b1d36173c31f1e1bac2c408a4ea48f7
DSand313 <104176983+DSand313@users.noreply.github.com>
2022-05-02 09:56:03 -0500

    Update Advanced SQL Analytics.sql

--- Advanced SQL Analytics.sql
+++ Advanced SQL Analytics.sql
@@ -10,27 +10,27 @@ Analytics performed with sample databases
 USE sql_invoicing;
 CREATE VIEW sales_by_client AS 
 SELECT
-	c.client_id,
+    c.client_id,
     c.name,
     SUM(invoice_total) AS total_sales
 FROM clients c
 JOIN invoices i 
-	USING (client_id)
+    USING (client_id)
 GROUP BY 
-	client_id,
+    client_id,
     name;
     
 USE sql_invoicing;
 CREATE VIEW clients_balance AS
 SELECT 
-	c.client_id,
+    c.client_id,
     c.name,
     SUM(invoice_total - payment_total) AS balance
 FROM clients c
 JOIN invoices i
 	USING (client_id)
 GROUP BY
-	client_id,
+    client_id,
     name;
     
 -- 2. Stored procedures
@@ -41,8 +41,8 @@ DELIMITER $$
 
 CREATE PROCEDURE get_clients()
 BEGIN
-	SELECT * 
-	FROM clients;
+    SELECT * 
+    FROM clients;
 END$$
 
 DELIMITER ; 
@@ -52,7 +52,7 @@ DELIMITER $$
 
 CREATE PROCEDURE get_invoices_with_balance()
 BEGIN
-	SELECT *
+    SELECT *
     FROM invoices_with_balance
     WHERE balance > 0;
 END$$
@@ -68,10 +68,10 @@ DELIMITER $$
 DROP TRIGGER IF EXISTS payment_after_insert;
 
 CREATE TRIGGER payments_after_insert
-	AFTER INSERT ON payments
+    AFTER INSERT ON payments
     FOR EACH ROW
 BEGIN
-	UPDATE invoices
+    UPDATE invoices
     SET payment_total = payment_total + NEW.amount
     WHERE invoice_id = NEW.invoice_id;
 END$$
@@ -84,17 +84,17 @@ USE sql_invoicing;
 DELIMITER $$
 
 CREATE TRIGGER payments_after_insert
-	AFTER INSERT ON payments
-	FOR EACH ROW
+    AFTER INSERT ON payments
+    FOR EACH ROW
 BEGIN
-	UPDATE invoices
+    UPDATE invoices
     SET payment_total = payment_total + NEW.amount
     WHERE  invoice_id = NEW.invoice_id;
     
     INSERT INTO payments_audit
     VALUES (
-		NEW.client_id,
-		NEW.date,
+	NEW.client_id,
+	NEW.date,
         NEW.amount,
         "Insert",
         NOW()
@@ -111,10 +111,10 @@ DELIMITER $$
 
 CREATE EVENT yearly_delete_stale_audit_rows
 ON SCHEDULE
-	-- AT "2019-05-01"
+    -- AT "2019-05-01"
     EVERY 1 YEAR STARTS "2019-01-01" ENDS "2029-01-01"
 DO BEGIN 
-	DELETE FROM payments_audit
+    DELETE FROM payments_audit
     WHERE action_date < NOW() - INTERVAL 1 YEAR;
 END$$
 
@@ -127,19 +127,19 @@ USE sql_store;
 START TRANSACTION;
 
 INSERT INTO orders (
-	customer_id,
+    customer_id,
     order_date,
     status
 )
 VALUES (
-	1,
+    1,
     "2019-01-01",
     1
 );
 
 INSERT INTO order_items
 VALUES (
-	last_insert_id(),
+    last_insert_id(),
     1,
     1,
     1
