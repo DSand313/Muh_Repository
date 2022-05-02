7680abbb5279da17adbb5c91c72bfff297060a8d
DSand313 <104176983+DSand313@users.noreply.github.com>
2022-05-02 10:01:51 -0500

    Update Basic_SQL_Analytics.sql

--- Basic_SQL_Analytics.sql
+++ Basic_SQL_Analytics.sql
@@ -1,44 +1,46 @@
--- joins
+-- 1. Joins
+
 USE sql_store;
 SELECT *
 FROM orders o
 JOIN customers c
-	USING (customer_id)
+    USING (customer_id)
 JOIN order_statuses os
-	ON o.status = os.order_status_id;
+    ON o.status = os.order_status_id;
 
--- subqueries
+-- 2. Subqueries
+
 USE sql_invoicing;
 SELECT * 
 FROM clients
 WHERE client_id IN (
-	SELECT client_id
+    SELECT client_id
     FROM invoices
     GROUP BY client_id
     HAVING COUNT(*) > 2
 );
 
--- correlated subqueries
+-- 3. Correlated subqueries
 USE sql_hr;
 SELECT * 
 FROM employees e
 WHERE salary > (
-	SELECT AVG(salary)
+    SELECT AVG(salary)
     FROM employees
     WHERE office_id = e.office_id
 );
 
--- unions
+-- 4. Unions
 USE sql_store;
 SELECT 
-	order_id,
+    order_id,
     order_date,
     "Active" AS status
 FROM orders
 WHERE order_date >= "2019-01-01"
 UNION
 SELECT 
-	order_id,
+    order_id,
     order_date,
     "Archived" AS status
 FROM orders
